7a4f7d70a81e951f0f605be9f414db1cd9433c35
tugotron <ufo6515@gmail.com>
2015-07-26 16:19:46 +0300

    up

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,6 +36,7 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     ['label' => 'Home', 'url' => ['/site/index']],
+                    ['label' => 'Add', 'url' => ['post/add']],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
 
